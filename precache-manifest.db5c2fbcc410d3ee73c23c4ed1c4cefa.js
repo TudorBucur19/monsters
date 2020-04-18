fbf750fcecb5bfa45e7c3c2a893f05bb86d27b27
TudorBucur19 <tudor.bucur88@gmail.com>
2020-04-18 17:08:21 +0300

    Updates

---
+++ precache-manifest.db5c2fbcc410d3ee73c23c4ed1c4cefa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874f483ca05952f56f5c2c793601019a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "75e763da3e2421bc9d5a",
+    "url": "/monsters/static/css/main.65b5b52d.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e763da3e2421bc9d5a",
+    "url": "/monsters/static/js/main.c8bee639.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
